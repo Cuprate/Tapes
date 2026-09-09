64d747e169c0dacfa544ace6b4bee82b76f6f150
Boog900 <54e72d8a-345f-4599-bd90-c6b9bc7d0ec5@aleeas.com>
2026-09-09 21:37:15 +0100

    remove test for behaviour not allowed

--- tests/delete_tape.rs
+++ tests/delete_tape.rs
@@ -37,38 +37,3 @@ fn delete_blob_tape() {
         assert!(!append.tape_exists("blob"));
     }
 }
-
-#[test]
-fn delete_wins_over_modification_in_the_same_transaction() {
-    let dir = tempfile::tempdir().unwrap();
-    let tapes = Tapes::open(dir.path()).unwrap();
-
-    let options = WholeTapeOpenOptions {
-        dir: dir.path().to_path_buf(),
-    };
-
-    let mut append = tapes.append();
-    let tape = append
-        .open_blob_tape::<WholeBlobTape>("blob", options.clone())
-        .unwrap();
-    append.append_bytes(&tape, b"contents").unwrap();
-    append.commit(Persistence::Buffer).unwrap();
-
-    let tape_dir = dir.path().join("tapes").join("blob");
-    assert!(tape_dir.exists());
-
-    let mut append = tapes.append();
-    let tape = append
-        .open_blob_tape::<WholeBlobTape>("blob", options)
-        .unwrap();
-    append.append_bytes(&tape, b"more").unwrap();
-    append.delete_tape(tape);
-    append.commit(Persistence::Buffer).unwrap();
-
-    assert!(!tape_dir.exists());
-
-    {
-        let append = tapes.append();
-        assert!(!append.tape_exists("blob"));
-    }
-}
